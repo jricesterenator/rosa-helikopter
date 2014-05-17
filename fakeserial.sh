080e2fc86da7316ff36b2e6b0a7946552e67ed81
jjrice <jrmailings@gmail.com>
2014-05-16 19:16:32 -0500

    added description and usage to serial connector

--- fakeserial.sh
+++ fakeserial.sh
@@ -1,1 +1,15 @@
+#
+# Use this script to create a software serial connection between 2 mock
+# devices.
+#
+# This useful for integration testing when it's not possible to connect to the
+# real car. The quadcopter program can connect to a mock car (can_simulator.py)
+# over serial. The mock car pumps mock CAN messages over the serial link.
+#
+# Usage:
+#   1. Run this to setup the serial link.
+#   2. Start the mock car/CAN simulator (can_simulator.py)
+#   3. Start the quadcopter program.
+#   4. Use the CAN simulator to pump commands or manually send them one-by-one
+#
 socat PTY,link=/tmp/fake1,raw,echo=0 PTY,link=/tmp/fake2,raw,echo=0
